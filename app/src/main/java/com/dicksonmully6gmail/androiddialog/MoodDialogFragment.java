97488adb73eab1c505afa31c71982bb556c45505
dicksonmuli <dicksonmully6@gmail.com>
2017-05-25 12:14:22 +0300

    submit button onclick listener showing the checked button on the android logs

--- app/src/main/java/com/dicksonmully6gmail/androiddialog/MoodDialogFragment.java
+++ app/src/main/java/com/dicksonmully6gmail/androiddialog/MoodDialogFragment.java
@@ -2,10 +2,14 @@ package com.dicksonmully6gmail.androiddialog;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 /**
  * Created by dickson on 5/25/17.
@@ -13,6 +17,22 @@ import android.widget.Button;
 
 public class MoodDialogFragment extends DialogFragment {
 
+//    public void onRadioButtonClicked(View view) {
+//        // Is the button now checked?
+//        boolean checked = ((RadioButton) view).isChecked();
+//
+//        // Check which radio button was clicked
+//        switch(view.getId()) {
+//            case R.id.radioButton1:
+//                if (checked)
+//                    dismiss();
+//                    break;
+//            case R.id.radioButton2:
+//                if (checked)
+//                    // Ninjas rule
+//                    break;
+//        }
+//    }
 
 
 
@@ -23,7 +43,24 @@ public class MoodDialogFragment extends DialogFragment {
         getDialog().setTitle("Simple Dialog");
 
         Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
-        Button submitbutton = (Button) rootView.findViewById(R.id.submitButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+        RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup); //pull group
+        final int selectedId = surveyRadioGroup.getCheckedRadioButtonId(); //get selected ID
+        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId); //get r button val via ID
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("testing", selectedRadioButton.getText().toString());
+//                View radioButton = selectedRadioButton.findViewById(selectedId);
+//                Toast.makeText(getApplicationContext(), "Your mood is " + radioButton).show();
+
+                // Add logic here
+
+                dismiss();
+            }
+        });
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +69,8 @@ public class MoodDialogFragment extends DialogFragment {
             }
         });
         return rootView;
+
+
 
     }
 
