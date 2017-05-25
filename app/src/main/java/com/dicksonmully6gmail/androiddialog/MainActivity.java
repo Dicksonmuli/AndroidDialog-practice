ca3f49af9d767bc485b84a0e884d8f11c5439d01
dicksonmuli <dicksonmully6@gmail.com>
2017-05-25 09:08:43 +0300

    add main activity and Fragment adapter for dialog. Fragment mood layout to diasplay the different radio button to select mood

---
+++ app/src/main/java/com/dicksonmully6gmail/androiddialog/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dicksonmully6gmail.androiddialog;
+
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager();
+        MoodDialogFragment moodDialogFragment = new MoodDialogFragment ();
+        moodDialogFragment.show(fm, "Sample Fragment");
+    }
+
+
+}
---
+++ app/src/main/java/com/dicksonmully6gmail/androiddialog/MoodDialogFragment.java
@@ -0,0 +1,22 @@
+package com.dicksonmully6gmail.androiddialog;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dickson on 5/25/17.
+ */
+
+public class MoodDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+        getDialog().setTitle("Simple Dialog");
+        return rootView;
+
+    }
+}
