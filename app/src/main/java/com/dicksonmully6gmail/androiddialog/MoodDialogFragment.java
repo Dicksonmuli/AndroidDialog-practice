9daadcf1dcc9ec5ab3709827cf26723b7d4adc9c
dicksonmuli <dicksonmully6@gmail.com>
2017-05-25 10:49:15 +0300

    function for cancel button to dismiss the dialog fragment if clicked

--- app/src/main/java/com/dicksonmully6gmail/androiddialog/MoodDialogFragment.java
+++ app/src/main/java/com/dicksonmully6gmail/androiddialog/MoodDialogFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by dickson on 5/25/17.
@@ -12,11 +13,27 @@ import android.view.ViewGroup;
 
 public class MoodDialogFragment extends DialogFragment {
 
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
         getDialog().setTitle("Simple Dialog");
+
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitbutton = (Button) rootView.findViewById(R.id.submitButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
         return rootView;
 
     }
+
+
 }
