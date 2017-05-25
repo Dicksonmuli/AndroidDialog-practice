8807a806d23c097bb9e0e957f787f7ac2b0ba7b6
dicksonmuli <dicksonmully6@gmail.com>
2017-05-25 10:03:50 +0300

    add a button on main activity to open a dialog when clicked

--- app/src/main/java/com/dicksonmully6gmail/androiddialog/MainActivity.java
+++ app/src/main/java/com/dicksonmully6gmail/androiddialog/MainActivity.java
@@ -1,22 +1,38 @@
 package com.dicksonmully6gmail.androiddialog;
 
 import android.app.FragmentManager;
+import android.app.IntentService;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Bind(R.id.moodButton)
+    Button mDialogButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        FragmentManager fm = getFragmentManager();
-        MoodDialogFragment moodDialogFragment = new MoodDialogFragment ();
-        moodDialogFragment.show(fm, "Sample Fragment");
+        mDialogButton.setOnClickListener(this);
+    }
+    @Override
+    public  void onClick(View v) {
+        if(v == mDialogButton) {
+            FragmentManager fm = getFragmentManager();
+            MoodDialogFragment moodDialogFragment = new MoodDialogFragment ();
+            moodDialogFragment.show(fm, "Sample Fragment");
+        }
     }
 
 
